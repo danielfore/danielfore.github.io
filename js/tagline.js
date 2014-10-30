9a022ac39e3c3c39652c82d7b88879a6fa782eea
Daniel Foré <dan@chicagofire.com>
2014-10-30 16:23:27 -0700

    more taglines

--- js/tagline.js
+++ js/tagline.js
@@ -2,7 +2,12 @@ var textArray = [
     'War & Pizzas',
     'Mega Epic Pizza Battle',
     'Pizza-Palooza',
-    "A Slice o' The City"
+    "A Slice o' The City",
+    'Not a Pizza Tour',
+    "Gotta Slice 'em All",
+    'Pizza For a Purpose',
+    'Eatsa Pizza',
+    "Sacramento's Best Pies"
 ];
 var randomNumber = Math.floor(Math.random()*textArray.length);
 
