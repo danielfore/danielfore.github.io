e671ab238a9b0f04f8e405eacf9a9b2fd20ea1e7
Daniel Foré <dan@chicagofire.com>
2014-11-04 13:11:30 -0800

    go back

--- ChiFiOnline/js/viewheight.js
+++
@@ -1,6 +0,0 @@
-$element = $('.view-height');
-function fixMobileSafariViewport() {
-  $element.css('min-height', window.innerHeight);
-}
-window.addEventListener('orientationchange', fixMobileSafariViewport, true);
-fixMobileSafariViewport();
--- ChiFiOnline/new/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    © West of Chicago Restaurants Inc. 2014 All Rights Reserved
-    <a href="/privacy.php">Privacy Policy</a>
-</footer>
